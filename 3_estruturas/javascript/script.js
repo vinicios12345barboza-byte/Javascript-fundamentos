8920903c0b64c3454e5db66139f570aa659c363a
Vinicios Barboza <vinicios12345barboza@gmail.com>
2026-04-07 08:04:11 -0300

    Estruturas JS

---
+++ 3_estruturas/javascript/script.js
@@ -0,0 +1,38 @@
+// 1 - Variáveis
+let nome = "Vinicios";
+console.log(nome);
+
+nome = "Sarah";
+console.log(nome)
+
+const idade = 19;
+console.log(idade)
+
+const idadee = 18;
+console.log(idadee, nome);
+
+// idade = 20; - const não pode mudar 
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+// 2 - Mais sobre variáveis
+// let 2teste = "Inválido"
+// let @teste = "Inválido"
+
+let a = 10, b =20, c = 30
+
+console.log(a, b, c);
+
+const nomecompleto = "Vinicios henrique Barboza"
+
+const nomeCompleto = "Sarah vieira castro"
+
+console.log(nomecompleto);
+console.log(nomeCompleto);
+
+// simbolos permitidos para variáveis
+let _teste = "ok";
+let $teste = "ok";
+
+console.log(_teste, $teste);
